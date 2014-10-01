d373344713f270059414970f22591b6fb1144031
unknown <Attila Papai>
2014-10-01 17:45:54 +0200

    Hetedik feladat

--- src/sorszuru/Main.java
+++ src/sorszuru/Main.java
@@ -2,10 +2,17 @@ package sorszuru;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.Reader;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class Main {
 
@@ -18,6 +25,8 @@ public class Main {
 			String input = null;
 			String output = null;
 			String pattern = "";
+			String outputGZIP = null;
+			String inputGZIP = null;
 			for (int i = 0; i < args.length; i++) {
 				if ((i+1 < args.length) && args[i].equals("-i")) {
 					i++;
@@ -28,11 +37,29 @@ public class Main {
 				} else if ((i+1 < args.length) && args[i].equals("-p")) {
 					i++;
 					pattern = args[i];
+				} else if ((i+1 < args.length) && args[i].equals("-gi")) {
+					i++;
+					inputGZIP = args[i];
+				} else if ((i+1 < args.length) && args[i].equals("-go")) {
+					i++;
+					outputGZIP = args[i];
 				}
+				
+				
 			}
-			
-			FileReader fr = new FileReader(new File(wd, input));
-			BufferedReader br = new BufferedReader(fr);
+			BufferedReader br;
+			if (input != null)
+			{
+				FileReader fr = new FileReader(new File(wd, input));
+				br = new BufferedReader(fr);
+			}
+			else
+			{
+				FileInputStream fr = new FileInputStream(inputGZIP);
+				GZIPInputStream g = new GZIPInputStream(fr);
+				Reader decoder = new InputStreamReader(g, "UTF-8");
+				br = new BufferedReader(decoder);
+			}
 			while(true)
 			{
 				String line = br.readLine();
@@ -40,8 +67,19 @@ public class Main {
 					break;
 				if (line.matches(pattern))
 				{
-					FileWriter fw = new FileWriter(output, true);
-					PrintWriter pw = new PrintWriter(fw);
+					PrintWriter pw;
+					if (output != null)
+					{
+						FileWriter fw = new FileWriter(output, true);
+						pw = new PrintWriter(fw);
+					}
+					else
+					{
+						FileOutputStream fo = new FileOutputStream(outputGZIP, true);
+						GZIPOutputStream go = new GZIPOutputStream(fo);
+						OutputStreamWriter o = new OutputStreamWriter(go);
+						pw = new PrintWriter(o, true);
+					}
 					pw.println(line);
 					pw.close();
 				}
